29a318f64e09a663fdc44f184012e69a4a73bc3a
PrecieuxMudibu <oceangpmb@gmail.com>
2023-04-02 11:25:47 +0100

    Create tarrif model

---
+++ models/tariffModel.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import mongooseUniqueValidator from 'mongoose-unique-validator';
+
+const { Schema, model } = mongoose;
+
+const tariffSchema = new Schema(
+    {
+        name: { type: String, unique: true },
+    },
+    { timestamps: true }
+);
+
+tariffSchema.plugin(mongooseUniqueValidator);
+
+const User = model('User', tariffSchema);
+export default User;
