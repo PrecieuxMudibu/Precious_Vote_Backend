cf1604148e6a83b269b977d0fc8d88f3a5be5eda
PrecieuxMudibu <oceangpmb@gmail.com>
2023-04-03 22:29:28 +0100

    Add todos comment for the vote candidate in the candidateControllers.js

--- controllers/candidateControllers.js
+++ controllers/candidateControllers.js
@@ -14,6 +14,9 @@ function get_candidates(request, response, next) {
 function vote_candidate(request, response) {
     const { candidate_id, elector_id, round_id } = request.body;
 
+    // TODO : ADD A CODE FOR VERIFY IF THE ROUND IS CLOSED
+    // If it is finished vote with the second round_id if it exists
+
     const vote = new ElectorCandidateRound({
         candidate_id: candidate_id,
         elector_id: elector_id,
