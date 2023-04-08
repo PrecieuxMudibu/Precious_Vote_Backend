423d6a87af978c388d2a20ae4c93ca691d59aadc
PrecieuxMudibu <oceangpmb@gmail.com>
2023-04-08 16:55:24 +0100

    feat: Close a round with his id

--- controllers/roundControllers.js
+++ controllers/roundControllers.js
@@ -20,22 +20,6 @@ function create_round(request, response, next) {
         .catch((error) => response.status(400).json({ error }));
 }
 
-// const Character = mongoose.model(
-//     'Character',
-//     new mongoose.Schema({
-//         name: String,
-//         age: Number,
-//     })
-// );
-
-// await Character.create({ name: 'Jean-Luc Picard' });
-
-// const filter = { name: 'Jean-Luc Picard' };
-// const update = { age: 59 };
-
-// // `doc` is the document _before_ `update` was applied
-// let doc = await Character.findOneAndUpdate(filter, update);
-
 function start_round(request, response, next) {
     const { round_id } = request.body;
 
@@ -51,4 +35,20 @@ function start_round(request, response, next) {
         )
         .catch((error) => response.status(400).json({ error }));
 }
-export { create_round, start_round };
+
+function close_round(request, response, next) {
+    const { round_id } = request.body;
+
+    const filter = { _id: round_id };
+    const update = { status: 'Completed', end_date: Date.now() };
+
+    Round.findOneAndUpdate(filter, update)
+        .then((round) =>
+            response.status(200).json({
+                message: 'Le round est terminé.',
+                round,
+            })
+        )
+        .catch((error) => response.status(400).json({ error }));
+}
+export { create_round, start_round, close_round };
--- routes/roundRoutes.js
+++ routes/roundRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { create_round, start_round } from '../controllers/roundControllers.js';
+import { create_round, start_round, close_round } from '../controllers/roundControllers.js';
 
 const router = express.Router();
 
 router.post('/create_round', create_round);
 router.put('/start_round', start_round);
+router.put('/close_round', close_round);
 
 export default router;
