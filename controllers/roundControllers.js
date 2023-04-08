5a929a477642e5e37007b9ae0e7c0575591a27ab
PrecieuxMudibu <oceangpmb@gmail.com>
2023-04-08 21:13:57 +0100

    feat: retrieve election_id for a round

--- controllers/roundControllers.js
+++ controllers/roundControllers.js
@@ -1,4 +1,10 @@
 import Round from '../models/roundModel.js';
+import Elector from '../models/electorModel.js';
+
+function get_election_id_of_this(round) {
+    const election_id = round.post_id.election_id;
+    return election_id;
+}
 
 function create_round(request, response, next) {
     const { number, status, election_id } = request.body;
@@ -27,12 +33,37 @@ function start_round(request, response, next) {
     const update = { status: 'In progress', begin_date: Date.now() };
 
     Round.findOneAndUpdate(filter, update)
-        .then((round) =>
-            response.status(200).json({
-                message: 'Le round a commencé.',
-                round,
-            })
-        )
+        .populate('post_id')
+        .then((round) => {
+            // response.status(200).json({
+            //     message: 'Le round a commencé.',
+            //     round,
+            // })
+            const election_id = get_election_id_of_this(round);
+
+            Elector.find({ election_id: election_id }).then((electors) =>
+                response.status(200).json({
+                    // message: 'Le round a commencé.',
+                    electors,
+                })
+            );
+
+            // GET ELECTION ID OF THIS POST
+
+            // {
+            //     _id: new ObjectId("64313d858f49528385eb03b3"),
+            //     post_id: new ObjectId("64313d838f49528385eb03a8"),
+            //     number: 1,
+            //     status: 'Not started',
+            //     createdAt: 2023-04-08T10:10:13.115Z,
+            //     updatedAt: 2023-04-08T10:10:13.115Z,
+            //     __v: 0
+            //   }
+
+            // RETRIEVE ALL ELECTORS
+
+            // SEND THEM THEIR CODE BY EMAIL
+        })
         .catch((error) => response.status(400).json({ error }));
 }
 
