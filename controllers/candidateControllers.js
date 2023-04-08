d7f5208c3143c7c0a6186344f9f6e9b1d36a920d
PrecieuxMudibu <oceangpmb@gmail.com>
2023-04-08 16:14:46 +0100

    feat: Can't vote if the round 1 are not in progress

--- controllers/candidateControllers.js
+++ controllers/candidateControllers.js
@@ -1,6 +1,7 @@
 import Candidate from '../models/candidateModel.js';
 import ElectorCandidateRound from '../models/electorCandidateRoundModel.js';
 import Round from '../models/roundModel.js';
+import Elector from '../models/electorModel.js';
 
 function get_candidates(request, response, next) {
     const { election_id, post_id } = request.params;
@@ -15,37 +16,102 @@ function get_candidates(request, response, next) {
 function vote_candidate(request, response) {
     const { candidate_id, token_for_vote } = request.body;
 
-    Candidate.findOne({ _id: candidate_id })
-        // .then((candidate) => response.status(200).json({ candidate }))
-        .then((candidate) => {
-            // RETRIEVE ROUNDS FOR THIS POST
-            const post_id = candidate.post_id;
-            Round.find({ post_id: post_id })
-                .then((rounds) => {
-                    let round_1 = rounds.filter((round) => round.number == 1);
-                    round_1 = round_1[0];
-                    let round_2 = rounds.filter((round) => round.number == 2);
-                    round_2 = round_2[0];
-                })
-                .catch((error) => response.status(500).json({ error }));
+    Elector.findOne({ token_for_vote: token_for_vote })
+        .then((elector) => {
+            if (elector == null) {
+                return response
+                    .status(404)
+                    .json({ message: 'Aucun électeur trouvé avec ce token' });
+            } else {
+                // return response.status(200).json({ elector });
+                Candidate.findOne({ _id: candidate_id })
+                    .then((candidate) => {
+                        if (candidate == null) {
+                            return response.status(404).json({
+                                message: 'Aucun candidat trouvé avec cet id',
+                            });
+                        } else {
+                            // RETRIEVE ROUNDS FOR THIS POST
+                            Round.find({ post_id: candidate.post_id })
+                                .then((rounds) => {
+                                    let round_1 = rounds.filter(
+                                        (round) => round.number == 1
+                                    );
+                                    round_1 = round_1[0];
 
-            // FIND ROUNDS FOR AN ELECTION
+                                    let round_2 = rounds.filter(
+                                        (round) => round.number == 2
+                                    );
+                                    round_2 = round_2[0];
+
+                                    if (round_1.status == 'In progress') {
+                                        // SAVE THE VOTE
+                                        ElectorCandidateRound.find({
+                                            elector_id: elector._id,
+                                            round_id: round_1._id,
+                                        })
+                                            .then((electorCandidateRound) => {
+                                                // THE ELECTOR DOESN'T VOTE BEFORE FOR  THIS ROUND AND THIS POST
+                                                if (
+                                                    electorCandidateRound ==
+                                                    null
+                                                ) {
+                                                    const vote =
+                                                        new ElectorCandidateRound(
+                                                            {
+                                                                elector_id:
+                                                                    elector._id,
+                                                                candidate_id:
+                                                                    candidate._id,
+                                                                round_id:
+                                                                    round_1._id,
+                                                            }
+                                                        );
+                                                    vote.save(vote)
+                                                        .then((vote) =>
+                                                            response
+                                                                .status(201)
+                                                                .json({
+                                                                    message:
+                                                                        'Votre vote a été enregistré avec succès',
+                                                                })
+                                                        )
+                                                        .catch((error) =>
+                                                            response
+                                                                .status(500)
+                                                                .json({ error })
+                                                        );
+                                                } else {
+                                                    // THE ELECTOR HAS VOTED BEFORE FOR  THIS ROUND AND THIS POST
+                                                    return response
+                                                        .status(200)
+                                                        .json({
+                                                            message:
+                                                                'Vous avez déjà voté',
+                                                        });
+                                                }
+                                            })
+                                            .catch((error) =>
+                                                response
+                                                    .status(500)
+                                                    .json({ error })
+                                            );
+                                    } else {
+                                        //  Code 403 : Access refused
+                                        response.status(403).json({
+                                            message:
+                                                "Vous ne pouvez pas voter. Le round 1 est soit terminé, soit il n'a pas encore commencé.",
+                                        });
+                                    }
+                                })
+                                .catch((error) =>
+                                    response.status(500).json({ error })
+                                );
+                        }
+                    })
+                    .catch((error) => response.status(500).json({ error }));
+            }
         })
-
         .catch((error) => response.status(500).json({ error }));
-
-    // TODO : ADD A CODE FOR VERIFY IF THE ROUND IS CLOSED
-
-    // If it is finished vote with the second round_id if it exists
-
-    // const vote = new ElectorCandidateRound({
-    //     candidate_id: candidate_id,
-    //     elector_id: elector_id,
-    //     round_id: round_id,
-    // });
-
-    // vote.save()
-    //     .then((vote) => response.status(201).json({ vote }))
-    //     .catch((error) => response.status(500).json({ error }));
 }
 export { get_candidates, vote_candidate };
